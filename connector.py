ce8b071a81af025cd0d693c6bcdad27a611960f4
Albertus Agung <albertus.ag@outlook.com>
2021-01-16 15:00:49 +0900

    add testing code for socket connection

---
+++ connector.py
@@ -0,0 +1,38 @@
+# for testing purpose
+
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 13000        # The port used by the server
+
+# text = "Bytes are the actual data. Strings are an abstraction."
+# type = "message"
+
+text = "clear"
+type = "command"
+
+mark = "<UNK>"
+if type == "message":
+    mark = "<EOF>"
+elif type == "command":
+    mark = "<COM>"
+
+send_data = text + mark
+
+print(send_data)
+
+# for i in range(10):
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.connect((HOST, PORT))
+#         s.sendall(send_data.encode())
+#         data = s.recv(1024)
+#
+#     print('Received', repr(data))
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(send_data.encode())
+    data = s.recv(1024)
+
+print('Received', repr(data))
